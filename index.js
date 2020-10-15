c18786de5f14673136c30121c8340b2f2f4bfb0f
Michał Hajok <hajokmichal@gmail.com>
2020-10-15 20:49:36 +0200

    bug url

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-process.env.mongoose
+mongoose
   .connect(url, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
